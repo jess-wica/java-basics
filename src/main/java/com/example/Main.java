53f7289efe3c01e6e5d0b8f0dd92fcc2cc834165
Dan Foley <>
2019-09-24 22:04:59 +0100

    Minor tweaks

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,17 +9,18 @@ public class Main {
 
     public static void main(String[] args) {
         //Nothing to go here
+        // No need to put any code in this method
     }
 
     public String condition(String item) {
         return null;
     }
 
-    public List<String> lists(String... item) {
+    public List<String> lists(String... items) {
         return null;
     }
 
-    public Map<String, String> maps(String key, String items) {
+    public Map<String, String> maps(String key, String item) {
         return null;
     }
 
