c97c176fd83c90af81f56c899adbf10c9bf6437f
Jessica McKenzie <contact@jessmckenzie.com>
2019-09-26 11:26:42 +0100

    Add all solution attempts except complex maps

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,6 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
@@ -13,15 +10,39 @@ public class Main {
     }
 
     public String condition(String item) {
-        return null;
+        if (item == null) {
+            return "One";
+        } else {
+            if (item.equals("Number")) {
+                return "Two";
+            } else if (item.equals("Another")) {
+                return "Three";
+            } else if (item.isEmpty()) {
+                return "Four";
+            } else {
+                return "Five";
+            }
+        }
     }
 
     public List<String> lists(String... items) {
-        return null;
+        List<String> newList;
+        if(items == null) {
+            return Collections.EMPTY_LIST;
+        } else {
+                newList = Arrays.asList(items);
+                return newList;
+        }
     }
 
     public Map<String, String> maps(String key, String item) {
-        return null;
+        Map<String, String> myMap = new HashMap<>();
+        if (item == null || key == null) {
+            return Collections.EMPTY_MAP;
+        } else {
+            myMap.put(key, item);
+            return myMap;
+        }
     }
 
     public Map<String, List<String>> complexMaps(String key, String... items) {
